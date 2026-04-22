32d7f66ca6cf5ada2b425c974260e7d17de694ba
Victoria Meira <victoria.meira@al.infnet.edu.br>
2026-04-22 19:22:02 -0300

    Adiciona exercício 06 cálculo de dobro e triplo

---
+++ lista-01/ex06-dobro-triplo/script.js
@@ -0,0 +1,11 @@
+// Exercício 06
+// Recebe número do usuário e mostra dobro e triplo
+
+let primeiroNumero = prompt("Digite o primeiro número:");
+let primeiroNumeroConvertido = Number(primeiroNumero);
+
+alert(
+  `Dobro: ${primeiroNumeroConvertido * 2}\nTriplo: ${
+    primeiroNumeroConvertido * 3
+  }`
+);
