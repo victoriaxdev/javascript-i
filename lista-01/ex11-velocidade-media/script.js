6aecf7a0c1a3e83c13e1b1846e782c7d1f6d7665
Victoria Meira <victoria.meira@al.infnet.edu.br>
2026-04-22 19:41:02 -0300

    Adiciona exercício 11 cálculo de velocidade média

---
+++ lista-01/ex11-velocidade-media/script.js
@@ -0,0 +1,10 @@
+// Exercício 11
+// Calcula velocidade média: distância / tempo
+
+let distanciaString = prompt("Valor da distância:");
+let tempoString = prompt("Valor do tempo:");
+
+let distancia = Number(distanciaString);
+let tempo = Number(tempoString);
+
+alert("Velocidade Média: " + distancia / tempo);
