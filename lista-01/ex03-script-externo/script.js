970c393a6391babe06fd21f9c5cf86d1c3a468f3
Victoria Meira <victoria.meira@al.infnet.edu.br>
2026-04-22 15:55:03 -0300

    Adiciona exercícios 02, 03 e 04 da lista 01

---
+++ lista-01/ex03-script-externo/script.js
@@ -0,0 +1,4 @@
+// Exercício 03
+// Mostra uma mensagem de alerta na tela
+
+alert("Olá mundo! Falo do arquivo script.js");
---
+++ lista-01/ex04-variavel-alert-console/script.js
@@ -0,0 +1,7 @@
+// Exercício 04
+// Armazena valor em variável e exibe no console e alert
+
+let MinhaNotaNoTP = "10";
+
+console.log(MinhaNotaNoTP);
+alert(MinhaNotaNoTP);
