fb16aff9725a450b23a0e95eddaf3c4db3499a1c
Victoria Meira <victoria.meira@al.infnet.edu.br>
2026-04-22 19:37:07 -0300

    Adiciona exercício 10 com dois scripts separados

---
+++ lista-01/ex10-dois-scripts/script1.js
@@ -0,0 +1,4 @@
+// Exercício 10 - Script 1
+// Exibe mensagem para o usuário
+
+alert("Olá usuário!");
---
+++ lista-01/ex10-dois-scripts/script2.js
@@ -0,0 +1,4 @@
+// Exercício 10 - Script 2
+// Exibe mensagem para o desenvolvedor no console
+
+console.log("Olá desenvolvedor!");
