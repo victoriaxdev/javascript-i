3a44d1c0b09ada5a80fd211a4f38529bcafb5a70
Victoria Meira <victoria.meira@al.infnet.edu.br>
2026-04-22 19:19:43 -0300

    Adiciona exercício 05 com alert, prompt e console

---
+++ lista-01/ex05-alert-prompt-console/script.js
@@ -0,0 +1,8 @@
+// Exercício 05
+// Exibe alerta, recebe entrada do usuário e mostra no console
+
+alert("Olá Mundo!");
+
+let resposta_usuario = prompt("Como você está hoje?");
+
+console.log(resposta_usuario);
