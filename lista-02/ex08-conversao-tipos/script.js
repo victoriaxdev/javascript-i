52f89673c1f21b74df961dd2a050cc04d99410e7
Victoria Meira <victoria.meira@al.infnet.edu.br>
2026-04-24 19:25:20 -0300

    Adiciona exercício 08 conversão de tipos

---
+++ lista-02/ex08-conversao-tipos/script.js
@@ -0,0 +1,10 @@
+// Exercício 08 - Lista 02
+// Demonstra conversões entre string e número
+
+const a = 1;
+const b = Number(a) + 2;
+const c = String(b) - 1;
+const d = String(c) * 1;
+const e = String(a) + Number(b) * d;
+
+console.log(e);
