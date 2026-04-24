86dc515876155a6f4d7fcaf9d85f2008a65101ce
Victoria Meira <victoria.meira@al.infnet.edu.br>
2026-04-24 19:26:47 -0300

    Adiciona exercício 09 condicional de idade

---
+++ lista-02/ex09-condicional-idade/script.js
@@ -0,0 +1,10 @@
+// Exercício 09 - Lista 02
+// Usa if/else para classificar idade
+
+const idade = Number(prompt("Qual a sua idade?"));
+
+if (idade >= 18) {
+  alert("Olá adulto!");
+} else {
+  alert("Olá jovem!");
+}
