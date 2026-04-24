7f6423dd21b226efac9ff5e2ff2767f0e259bd20
Victoria Meira <victoria.meira@al.infnet.edu.br>
2026-04-24 19:23:18 -0300

    Adiciona exercício 07 corrigindo nomes de variáveis

---
+++ lista-02/ex07-corrigindo-variaveis/script.js
@@ -0,0 +1,10 @@
+// Exercício 07 - Lista 02
+// Corrigindo nomes de variáveis para boas práticas (camelCase)
+
+const chicoBuarque = "Chico Buarque";
+const joaoGilberto = "João Gilberto";
+const marisaMonte = "Marisa Monte";
+
+const gunsNRoses = "Guns N Roses";
+const bulletForMyValentine = "Bullet for my Valentine";
+const charlieBrownJr = "Charlie Brown Jr";
