1a89c90d81d38bcc222015a08c93cfd0cb09e5c0
Victoria Meira <victoria.meira@al.infnet.edu.br>
2026-04-24 19:19:27 -0300

    Adiciona exercício 06 da lista 02 padrões de nomenclatura

---
+++ lista-02/ex06-nomenclatura-variaveis/script.js
@@ -0,0 +1,14 @@
+// Exercício 06 - Lista 02
+// Demonstra padrões de nomenclatura de variáveis
+
+// snake_case
+const chico_buarque = "Chico Buarque";
+const charlie_brown_jr = "Charlie Brown JR";
+
+// PascalCase
+const JoaoGilberto = "João Gilberto";
+const BulletForMyValentine = "Bullet for my Valentine";
+
+// camelCase
+const marisaMonte = "Marisa Monte";
+const gunsNroses = "Guns N Roses";
